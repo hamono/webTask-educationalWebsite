6bfce05f905cbe1e877c9432ab0fbeed376cd83c
hamono <487496627@qq.com>
2020-03-23 16:11:45 +0800

    修改

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -11,8 +11,6 @@ export default function App() {
     <Route component={All} />
   </Switch>
   </BrowserRouter>
-
-
 }
 
 
--- src/views/Up/index.tsx
+++ src/views/Up/index.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import * as style from "./style.scss";
 import { Carousel } from "antd";
+import img1 from "./img1.png";
+import img2 from "./img2.png";
+import img3 from "./img3.png";
 
 export default function Up() {
   return <div className={style.box}>
     <Carousel autoplay dotPosition="bottom" dots={true} className={style.main}>
       <div className={style.item}>
-        <h3>这里会有一些东西</h3>
+        <img src={img1} width="100%" height="350px" />
       </div>
       <div className={style.item}>
-        <h3>这里会有一些东西</h3>
+        <img src={img2} width="100%" height="350px" />
       </div>
       <div className={style.item}>
-        <h3>这里会有一些东西</h3>
-      </div>
-      <div className={style.item}>
-        <h3>这里会有一些东西</h3>
+        <img src={img3} width="100%" height="350px" />
       </div>
     </Carousel>
   </div>
