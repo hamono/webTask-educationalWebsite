ede78ecf98e0c1a2dd2932fac89ae64c07b4f02f
hamono <487496627@qq.com>
2020-05-13 15:47:54 +0800

    feat: 登录提醒

--- src/views/Body/ChildItem/index.tsx
+++ src/views/Body/ChildItem/index.tsx
@@ -1,31 +1,32 @@
 import React from "react";
 import * as style from "./style.scss";
 import { Link } from "react-router-dom";
-// import SignContext from "src/views/SigniIn/signInContext";
-// import Warning from "src/views/comonent/Alert/alert";
+import SignContext from "src/views/SigniIn/signInContext";
+import Warning from "src/views/comonent/Alert/alert";
 
 export default function ChildItem(props: {
   item: string,
   list: string[],
   tag: string
 }) {
-  // const { success } = React.useContext(SignContext);
-  // const link = success ? '/playPage/' : '';
-  // const [signIn, setSignIn] = React.useState(true);
-  // const handleClick = React.useCallback(() => {
-  //   setSignIn(false)
-  // }, [])
+  const { success } = React.useContext(SignContext);
+  const link = success ? '/playPage/' : '';
+  const [signIn, setSignIn] = React.useState(true);
+  const handleClick = React.useCallback(() => {
+    setSignIn(false)
+  }, [])
+  console.log(signIn)
   return <div className={style.box}>
     <h1>{props.item}</h1>
     <div className={style.main}>
       <div className={style.img}>
-        <Link to='' ><div className={style.button}>进入直播</div></Link>
-        {/* {!success && !signIn && Warning()} */}
+        <Link to={link} onClick={handleClick} ><div className={style.button}>进入直播</div></Link>
+        {!success && !signIn && Warning()}
         <div className={style.logo}>{props.tag}</div>
       </div>
       <ul className={style.ul}>
         {props.list.map((v: {}, i: number) =>
-          <Link to="" key={i} >
+          <Link to="" key={i} onClick={handleClick} >
             <li className={style.li}>
               <div className={style.div} />{v}
             </li>
